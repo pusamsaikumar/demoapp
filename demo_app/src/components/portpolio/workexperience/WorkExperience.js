4bfe622422b46523cfe4d17f76ce7423166cb56e
pusamsaikumar <92392024+pusamsaikumar@users.noreply.github.com>
2025-03-25 22:46:18 +0530

    added work-experience details

--- demo_app/src/components/portpolio/workexperience/WorkExperience.js
+++ demo_app/src/components/portpolio/workexperience/WorkExperience.js
@@ -4,7 +4,7 @@ import { experienceData } from "../../../utlis/data";
 import ExperienceCard from "../ExperienceCard/ExperienceCard";
 import Slider from "react-slick";
 import slider from "react-slick/lib/slider";
-
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 const WorkExperience = () => {
   const sliderRef = useRef();
   var settings = {
@@ -25,10 +25,22 @@ const WorkExperience = () => {
       },
     ],
   };
+  const slideLeft = () => {
+    sliderRef.current.slickPrev();
+  };
+  const slideRight = () => {
+    sliderRef.current.slickNext();
+  };
   return (
     <section className="experience-container">
       <h5>Work Experience</h5>
       <div className="experience-content">
+        <div className="arrow-right" onClick={slideRight}>
+          <FaChevronRight />
+        </div>
+        <div className="arrow-left" onClick={slideLeft}>
+          <FaChevronLeft />
+        </div>
         <Slider ref={sliderRef} {...settings}>
           {experienceData?.length > 0 &&
             experienceData?.map((item, i) => (
